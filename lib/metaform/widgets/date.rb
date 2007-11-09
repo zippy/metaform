7d845143bb4f4d91f5850c40e0cc8e1891c9dc5a
mana <mana@8a44537b-53f4-0310-bd54-9ec50619bb65>
2007-11-09 15:30:31 +0000

    fixed but in date when value was not a legal date
    git-svn-id: svn+ssh://manastats.org/var/repos/mana/metaform/trunk/vendor/plugins/metaform@433 8a44537b-53f4-0310-bd54-9ec50619bb65

--- lib/metaform/widgets/date.rb
+++ lib/metaform/widgets/date.rb
@@ -3,13 +3,13 @@ class DateWidget < Widget
   ################################################################################
   def self.render_form_object(form,field_instance_id,value,options)
     require 'parsedate'
-    if value
-      date = Time.local(*ParseDate.parsedate(value))
-      <<-EOHTML
+    if value && (d = ParseDate.parsedate(value))[0]
+      date = Time.local(*d)
+      result = <<-EOHTML
       <input type="text" size=2 name="#{build_html_multi_name(field_instance_id,'month')}" id="#{build_html_multi_id(field_instance_id,'month')}" value="#{date.month}" /> /
       <input type="text" size=2 name="#{build_html_multi_name(field_instance_id,'day')}" id="#{build_html_multi_id(field_instance_id,'day')}" value="#{date.day}" /> /
       <input type="text" size=4 name="#{build_html_multi_name(field_instance_id,'year')}" id="#{build_html_multi_id(field_instance_id,'year')}" value="#{date.year}" />
-      EOHTML
+      EOHTML if 
     else
       <<-EOHTML
       <input type="text" size=2 name="#{build_html_multi_name(field_instance_id,'month')}" id="#{build_html_multi_id(field_instance_id,'month')}"/> /
