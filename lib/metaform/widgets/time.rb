2a1872ad4bb8e1936239e9f943cac8ebe366cd7c
mana <mana@8a44537b-53f4-0310-bd54-9ec50619bb65>
2007-12-04 18:01:50 +0000

    Lisa added am_pm to time widget
    git-svn-id: svn+ssh://manastats.org/var/repos/mana/metaform/trunk/vendor/plugins/metaform@629 8a44537b-53f4-0310-bd54-9ec50619bb65

--- lib/metaform/widgets/time.rb
+++ lib/metaform/widgets/time.rb
@@ -3,18 +3,32 @@ class TimeWidget < Widget
   ################################################################################
   def self.render_form_object(form,field_instance_id,value,options)
     form.time_select('record', field_instance_id)
+    meridian_options = [ ["AM","am"] , ["PM","pm"] ]
     require 'parsedate'
     if value
       d = *ParseDate.parsedate(value)
       date = Time.local(1,1,1,d[3],d[4])
+      
+      hours = date.hour
+      meridian = "am"
+      if (hours > 12)
+      	hours = hours - 12
+      	meridian =  "pm"
+	  end
       <<-EOHTML
-      <input type="text" size=2 name="#{build_html_multi_name(field_instance_id,'hours')}" id="#{build_html_multi_id(field_instance_id,'hours')}" value="#{date.hour}" /> :
+      <input type="text" size=2 name="#{build_html_multi_name(field_instance_id,'hours')}" id="#{build_html_multi_id(field_instance_id,'hours')}" value="#{hours}" /> :
       <input type="text" size=2 name="#{build_html_multi_name(field_instance_id,'minutes')}" id="#{build_html_multi_id(field_instance_id,'minutes')}" value="#{date.min}" />
+      <select name="#{build_html_multi_name(field_instance_id,'am_pm')}" id="#{build_html_multi_id(field_instance_id,'am_pm')}">
+      	#{form.options_for_select(meridian_options, meridian)}
+	  </select>
       EOHTML
     else
       <<-EOHTML
       <input type="text" size=2 name="#{build_html_multi_name(field_instance_id,'hours')}" id="#{build_html_multi_id(field_instance_id,'hours')}"/> :
       <input type="text" size=2 name="#{build_html_multi_name(field_instance_id,'minutes')}" id="#{build_html_multi_id(field_instance_id,'minutes')}"  />
+      <select name="#{build_html_multi_name(field_instance_id,'am_pm')}" id="#{build_html_multi_id(field_instance_id,'am_pm')}">
+	  	   #{form.options_for_select(meridian_options, "am")}
+	  </select>
       EOHTML
     end
   end
@@ -28,7 +42,7 @@ class TimeWidget < Widget
   ################################################################################
   def self.javascript_build_observe_function(field_instance_id,script,constraints)
     result = ""
-    %w(hours minutes).each do |field|
+    %w(hours minutes am_pm).each do |field|
       result << %Q|Event.observe('#{build_html_multi_id(field_instance_id,field)}', 'change', function(e){ #{script} });\n|
     end
     result
@@ -37,7 +51,8 @@ class TimeWidget < Widget
   ################################################################################
   def self.convert_html_value(value)
     begin
-      date = Time.local(1,1,1,value['hours'],value['minutes'])      
+      factor = value['am_pm'] == 'am' ? 0 : 12	
+      date = Time.local(1,1,1,value['hours'].to_i + factor,value['minutes'])
       date.strftime('%H:%M')
     rescue
       nil
