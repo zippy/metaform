61b158331653e9b4a4678de779e7c71e62255b35
mana <mana@8a44537b-53f4-0310-bd54-9ec50619bb65>
2007-12-12 15:43:43 +0000

    Lisa is checking in the changes to make optional_note work with notes_question
    git-svn-id: svn+ssh://manastats.org/var/repos/mana/metaform/trunk/vendor/plugins/metaform@648 8a44537b-53f4-0310-bd54-9ec50619bb65

--- lib/metaform/widgets/optional_note.rb
+++ lib/metaform/widgets/optional_note.rb
@@ -2,19 +2,24 @@
 class OptionalNoteWidget < Widget
   ################################################################################
   def self.render_form_object(form,field_instance_id,value,options)
-  	opts = options[:params] ? {:size => options[:params], :style=>'display:none'} : {:size => 25,:style=>'display:none'}
-    result = "&nbsp;&nbsp;"
-#    div_id = "#{field_instance_id}_optional_note"
-    div_id = build_html_name(field_instance_id)
-    result << form.link_to_function((value && value != '') ? 'Edit Note' : 'Add Note', 
-      "Effect.toggle($('#{div_id}'),'blind',{duration: .3});", #$('#{field_instance_id}_link').update('hide');
-      :id => "#{field_instance_id}_link")
-    result << <<-EOHTML
-   	#{form.text_field_tag(build_html_name(field_instance_id),value,opts)}
-    EOHTML
-    result
+  	params = options[:params]
+  	if params
+  		(size,inner_html) = params.split(/,/)
+		opts = size ? {:size => size, :style=>'display:none'} : {:size => 25,:style=>'display:none'}
+		inner_html = '' if !inner_html
+		result = "&nbsp;&nbsp;"
+		div_id = build_html_name(field_instance_id)
+		result << form.link_to_function((value && value != '') ? 'Edit Note' : 'Add Note', 
+		  "Effect.toggle($('#{div_id}'),'blind',{duration: .3});if(this.innerHTML=='Add Note') {this.innerHTML='Hide Note'} else {this.innerHTML='Add Note'};", #$('#{field_instance_id}_link').update('hide');
+		  :id => "#{field_instance_id}_link")
+		result << inner_html
+		result << <<-EOHTML
+		#{form.text_field_tag(build_html_name(field_instance_id),value,opts)}
+		EOHTML
+		result
+	end
   end
+  
     
 end
 ################################################################################
-
