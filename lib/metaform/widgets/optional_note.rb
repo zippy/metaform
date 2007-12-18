441d32862c2b00f737678007dfc29badb8670391
mana <mana@8a44537b-53f4-0310-bd54-9ec50619bb65>
2007-12-18 17:38:58 +0000

    Lisa is checking in work to make Add/Show/Hide work for optional notes
    git-svn-id: svn+ssh://manastats.org/var/repos/mana/metaform/trunk/vendor/plugins/metaform@656 8a44537b-53f4-0310-bd54-9ec50619bb65

--- lib/metaform/widgets/optional_note.rb
+++ lib/metaform/widgets/optional_note.rb
@@ -4,18 +4,35 @@ class OptionalNoteWidget < Widget
   def self.render_form_object(form,field_instance_id,value,options)
   	params = options[:params]
   	if params
-  		(size,inner_html) = params.split(/,/)
+  		(size,middle_html) = params.split(/,/)
 		opts = size ? {:size => size, :style=>'display:none'} : {:size => 25,:style=>'display:none'}
-		inner_html = '' if !inner_html
 		result = "&nbsp;&nbsp;"
 		div_id = build_html_name(field_instance_id)
-		result << form.link_to_function((value && value != '') ? 'Edit Note' : 'Add Note', 
-		  "Effect.toggle($('#{div_id}'),'blind',{duration: .3});if(this.innerHTML=='Add Note') {this.innerHTML='Hide Note'} else {this.innerHTML='Add Note'};", #$('#{field_instance_id}_link').update('hide');
-		  :id => "#{field_instance_id}_link")
-		result << inner_html
-		result << <<-EOHTML
-		#{form.text_field_tag(build_html_name(field_instance_id),value,opts)}
-		EOHTML
+		result << form.link_to_function('',"do_click_#{field_instance_id}()",:id => "#{field_instance_id}_link")
+		result << middle_html if middle_html
+		result << "#{form.text_field_tag(build_html_name(field_instance_id),value,opts)}"
+		js = <<-EOJS
+		function do_click_#{field_instance_id}() {
+			update_label_#{field_instance_id}(false);
+			Effect.toggle($('#{div_id}'),'blind',{duration: .3});
+		}
+		
+		function update_label_#{field_instance_id}(visible_bool) {
+		 var label;
+		 if( $('#{div_id}').visible() == visible_bool ) {
+		  	label='Hide Note';
+		  } else {
+		  	if( ($('#{div_id}').value)=='') {
+		  		label='Add Note';
+		  	} else {
+		  		label='Show Note';
+		  	}
+		  }		  
+		  $('#{field_instance_id}_link').innerHTML=label;
+		  }
+		  update_label_#{field_instance_id}(true);
+		EOJS
+		result << "#{form.javascript_tag(js)}"
 		result
 	end
   end
@@ -23,3 +40,7 @@ class OptionalNoteWidget < Widget
     
 end
 ################################################################################
+
+
+
+
