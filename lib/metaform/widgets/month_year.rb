5e87ac5c84fbca26cdb0eedc3ce257786b81f249
mana <mana@8a44537b-53f4-0310-bd54-9ec50619bb65>
2008-01-22 21:06:56 +0000

    lisa is checking in month_year widget
    git-svn-id: svn+ssh://manastats.org/var/repos/mana/metaform/trunk/vendor/plugins/metaform@699 8a44537b-53f4-0310-bd54-9ec50619bb65

--- lib/metaform/widgets/month_year.rb
+++ lib/metaform/widgets/month_year.rb
@@ -1,5 +1,5 @@
 ################################################################################
-class DateWidget < Widget
+class MonthYearWidget < Widget
   ################################################################################
   def self.render_form_object(form,field_instance_id,value,options)
     require 'parsedate'
@@ -7,14 +7,12 @@ class DateWidget < Widget
       date = Date.new(*d[0..2])
       <<-EOHTML
       <input type="text" size=2 class="textfield_2" name="#{build_html_multi_name(field_instance_id,'month')}" id="#{build_html_multi_id(field_instance_id,'month')}" value="#{date.month}" /> /
-      <input type="text" size=2 class="textfield_2" name="#{build_html_multi_name(field_instance_id,'day')}" id="#{build_html_multi_id(field_instance_id,'day')}" value="#{date.day}" /> /
-      <input type="text" size=4 class="textfield_4" name="#{build_html_multi_name(field_instance_id,'year')}" id="#{build_html_multi_id(field_instance_id,'year')}" value="#{date.year}" />
+      <input type="text" size=4 class="textfield_4" name="#{build_html_multi_name(field_instance_id,'year')}" id="#{build_html_multi_id(field_instance_id,'year')}" value="#{date.year}" /> (month/year)
       EOHTML
     else
       <<-EOHTML
       <input type="text" size=2 class="textfield_2" name="#{build_html_multi_name(field_instance_id,'month')}" id="#{build_html_multi_id(field_instance_id,'month')}"/> /
-      <input type="text" size=2 class="textfield_2" name="#{build_html_multi_name(field_instance_id,'day')}" id="#{build_html_multi_id(field_instance_id,'day')}"  /> /
-      <input type="text" size=4 class="textfield_4" name="#{build_html_multi_name(field_instance_id,'year')}" id="#{build_html_multi_id(field_instance_id,'year')}"  />
+      <input type="text" size=4 class="textfield_4" name="#{build_html_multi_name(field_instance_id,'year')}" id="#{build_html_multi_id(field_instance_id,'year')}"  /> (month/year)
       EOHTML
     end
   end
@@ -27,7 +25,7 @@ class DateWidget < Widget
   ################################################################################
   def self.javascript_build_observe_function(field_instance_id,script,constraints)
     result = ""
-    %w(month year day).each do |field|
+    %w(month year).each do |field|
       result << %Q|Event.observe('#{build_html_multi_id(field_instance_id,field)}', 'change', function(e){ #{script} });\n|
     end
     result
@@ -36,7 +34,7 @@ class DateWidget < Widget
   ################################################################################
   def self.convert_html_value(value,params={})
     begin
-      date = Date.new(value['year'].to_i,value['month'].to_i,value['day'].to_i)      
+      date = Date.new(value['year'].to_i,value['month'].to_i,1)      
       date.to_s
     rescue
       nil
