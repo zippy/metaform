bd229e9f4527dec42ea5e43aeef6f57c7667b7df
Eric Harris-Braun <eric@harris-braun.com>
2008-04-28 21:39:53 -0400

    added Height and WeightLkbkg widgets

---
+++ lib/metaform/widgets/height.rb
@@ -0,0 +1,72 @@
+################################################################################
+class HeightWidget < Widget
+  
+  ################################################################################
+  def self.render_form_object(form,field_instance_id,value,options)
+	params = options[:params]
+	js_update_height = <<-EOJS
+			function #{build_html_multi_id(field_instance_id,'update_height')}(change_meters) {
+					if (change_meters) {
+						var feet = parseFloat($F('#{build_html_multi_id(field_instance_id,'feet_box')}')); 
+						var inches = parseFloat($F('#{build_html_multi_id(field_instance_id,'inches_box')}')); 
+						var meters = Math.round((feet * 12 + inches) *  2.54)/100; 
+						$('#{build_html_multi_id(field_instance_id,'meters_box')}').value = meters;
+					} else {
+						var meters = parseFloat($F('#{build_html_multi_id(field_instance_id,'meters_box')}')); 
+						var total_inches = meters * 39.370079;
+						$('#{build_html_multi_id(field_instance_id,'feet_box')}').value = Math.floor(total_inches / 12);
+						$('#{build_html_multi_id(field_instance_id,'inches_box')}').value = Math.round(total_inches % 12);
+					}
+			}
+			EOJS
+	  if value 
+		meters = value.to_f.round.to_f / 100 # We store the value as centimeters	
+		total_inches = (meters * 39.370079).round
+		feet = total_inches / 12
+		inches = total_inches % 12      	
+		<<-EOHTML
+		<input type="text" size=2 class="textfield_2" name="#{build_html_multi_name(field_instance_id,'feet_box')}" id="#{build_html_multi_id(field_instance_id,'feet_box')}" value="#{feet}" onchange="#{build_html_multi_id(field_instance_id,'update_height')}(true)" /> ft
+		<input type="text" size=2 class="textfield_2" name="#{build_html_multi_name(field_instance_id,'inches_box')}" id="#{build_html_multi_id(field_instance_id,'inches_box')}" value="#{inches}" onchange="#{build_html_multi_id(field_instance_id,'update_height')}(true)" /> in or
+		<input type="text" size=4 class="textfield_4" name="#{build_html_multi_name(field_instance_id,'meters_box')}" id="#{build_html_multi_id(field_instance_id,'meters_box')}" value="#{meters}" onchange="#{build_html_multi_id(field_instance_id,'update_height')}(false)" /> m
+		#{form.javascript_tag(js_update_height)}
+		EOHTML
+	  else
+		<<-EOHTML
+		<input type="text" size=2 class="textfield_2" name="#{build_html_multi_name(field_instance_id,'feet_box')}" id="#{build_html_multi_id(field_instance_id,'feet_box')}" onchange="#{build_html_multi_id(field_instance_id,'update_height')}(true)" /> ft
+		<input type="text" size=2 class="textfield_2" name="#{build_html_multi_name(field_instance_id,'inches_box')}" id="#{build_html_multi_id(field_instance_id,'inches_box')}"  onchange="#{build_html_multi_id(field_instance_id,'update_height')}(true)" /> in
+		<input type="text" size=4 class="textfield_4" name="#{build_html_multi_name(field_instance_id,'meters_box')}" id="#{build_html_multi_id(field_instance_id,'meters_box')}"  onchange="#{build_html_multi_id(field_instance_id,'update_height')}(false)" /> m
+		#{form.javascript_tag(js_update_height)}
+		EOHTML
+	  end
+  end
+  
+  ################################################################################
+  def self.humanize_value(value,options=nil)
+    "#{value} meters"
+  end
+
+  ################################################################################
+  def self.javascript_get_value_function (field_instance_id)
+    %Q|$DF('#{build_html_id(field_instance_id)}')|
+  end
+
+  ################################################################################
+  def self.javascript_build_observe_function(field_instance_id,script,constraints)
+    result = ""
+    %w(feet_box inches_box meters_box).each do |field|
+      result << %Q|Event.observe('#{build_html_multi_id(field_instance_id,field)}', 'change', function(e){ #{script} });\n|
+    end
+    result
+  end
+
+  ################################################################################
+  def self.convert_html_value(value,params={})
+    begin
+   		result = value['meters_box'].to_f * 100 #Store as centimeters
+ 	rescue
+ 		nil
+    end
+  end
+
+end
+################################################################################
---
+++ lib/metaform/widgets/weight_lbkg.rb
@@ -0,0 +1,66 @@
+################################################################################
+class WeightLbkgWidget < Widget
+  
+  ################################################################################
+  def self.render_form_object(form,field_instance_id,value,options)
+	params = options[:params]
+	js_update_height = <<-EOJS
+			function #{build_html_multi_id(field_instance_id,'update_height')}(change_kilograms) {
+					if (change_kilograms) {
+						var pounds = parseFloat($F('#{build_html_multi_id(field_instance_id,'pounds_box')}')); 
+						var kilograms = Math.round(pounds *  4.5359237)/10; 
+						$('#{build_html_multi_id(field_instance_id,'kilograms_box')}').value = kilograms;
+					} else {
+						var kilograms = parseFloat($F('#{build_html_multi_id(field_instance_id,'kilograms_box')}')); 
+						var pounds = kilograms * 2.20462262;
+						$('#{build_html_multi_id(field_instance_id,'pounds_box')}').value = Math.round(pounds);
+					}
+			}
+			EOJS
+	  if value 
+		kilograms = (value.to_f / 100).round.to_f / 10 # We store the value as grams and want it displayed to 1 decimal places
+		pounds = (kilograms * 2.20462262).round
+		<<-EOHTML
+		<input type="text" size=2 class="textfield_4" name="#{build_html_multi_name(field_instance_id,'pounds_box')}" id="#{build_html_multi_id(field_instance_id,'pounds_box')}" value="#{pounds}" onchange="#{build_html_multi_id(field_instance_id,'update_height')}(true)" /> lb or
+		<input type="text" size=4 class="textfield_5" name="#{build_html_multi_name(field_instance_id,'kilograms_box')}" id="#{build_html_multi_id(field_instance_id,'kilograms_box')}" value="#{kilograms}" onchange="#{build_html_multi_id(field_instance_id,'update_height')}(false)" /> kg
+		#{form.javascript_tag(js_update_height)}
+		EOHTML
+	  else
+		<<-EOHTML
+		<input type="text" size=2 class="textfield_4" name="#{build_html_multi_name(field_instance_id,'pounds_box')}" id="#{build_html_multi_id(field_instance_id,'pounds_box')}"  onchange="#{build_html_multi_id(field_instance_id,'update_height')}(true)" /> lb
+		<input type="text" size=4 class="textfield_5" name="#{build_html_multi_name(field_instance_id,'kilograms_box')}" id="#{build_html_multi_id(field_instance_id,'kilograms_box')}"  onchange="#{build_html_multi_id(field_instance_id,'update_height')}(false)" /> kg
+		#{form.javascript_tag(js_update_height)}
+		EOHTML
+	  end
+  end
+  
+  ################################################################################
+  def self.humanize_value(value,options=nil)
+    "#{value} kilograms"
+  end
+
+  ################################################################################
+  def self.javascript_get_value_function (field_instance_id)
+    %Q|$DF('#{build_html_id(field_instance_id)}')|
+  end
+
+  ################################################################################
+  def self.javascript_build_observe_function(field_instance_id,script,constraints)
+    result = ""
+    %w(feet_box pounds_box kilograms_box).each do |field|
+      result << %Q|Event.observe('#{build_html_multi_id(field_instance_id,field)}', 'change', function(e){ #{script} });\n|
+    end
+    result
+  end
+
+  ################################################################################
+  def self.convert_html_value(value,params={})
+    begin
+   		result = value['kilograms_box'].to_f * 1000 #Store as grams
+ 	rescue
+ 		nil
+    end
+  end
+
+end
+################################################################################
