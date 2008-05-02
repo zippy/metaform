382ac278cf59da33cf9316a378702f9c6de8c775
Lisa Sawin <lsawin@optonline.net>
2008-05-02 09:47:13 -0400

    Updated checkbox group widgets to include javascript control over 'none' checkboxes and to use Scriptulous effects for better cross-platform followup display

--- lib/metaform/widgets/check_box_group.rb
+++ lib/metaform/widgets/check_box_group.rb
@@ -3,11 +3,50 @@ class CheckBoxGroupWidget < Widget
   ################################################################################
   def self.render_form_object(form,field_instance_id,value,options)
     e = enumeration(options[:constraints])
+    none_fields = []
+    e.map!{|value_label,val|
+      new_val = val.chomp('*')
+      if new_val != val || val == 'none'
+        none_fields << new_val
+      end
+      [value_label,new_val]
+    }
     checked = value.split(/,/) if value
     checked ||= []
     result = []
+    js_none = ""
+    none_fields.each { |none_val|
+      none_id = build_html_multi_id(field_instance_id,none_val)
+      js_none << <<-EOJS
+       if ($('#{none_id}')) {
+         $('#{none_id}').checked = false;
+       }
+       EOJS
+     }
+    js = <<-EOJS
+       function do_click_#{field_instance_id}_none(theCheckbox,theValue) {
+           if (theCheckbox.checked) {  
+             mapCheckboxGroup('#{build_html_name(field_instance_id)}',$('metaForm'),function(e,val){if (val != theValue) {e.checked=false}})
+           }
+    		}  
+    		function do_click_#{field_instance_id}_regular(theCheckbox,theValue,theFollowupID) {
+          var e = $(theFollowupID); 
+          if (theCheckbox.checked) {
+            #{js_none}
+          } else {
+            mapCheckboxGroup('record[#{field_instance_id}][_'+theValue+'-',$('metaForm'),function(el,val){el.checked=false})
+          }           
+   		  }
+        
+    	EOJS
     e.each do |key,val|
-      result << %Q|<input name="#{build_html_multi_name(field_instance_id,val)}" id="#{build_html_multi_id(field_instance_id,val)}" type="checkbox" value="#{val}" #{checked.include?(val) ? 'checked' : ''}> #{key}|
+      if none_fields.include?(val) 
+        # Javscript: uncheck all items in this checkbox group if the users clicks on none and also hide all the followups.
+        javascript = "do_click_#{field_instance_id}_none(this,'#{val}')"
+      else
+        javascript = "do_click_#{field_instance_id}_regular(this,'#{val}')"
+      end
+      result << %Q|<input name="#{build_html_multi_name(field_instance_id,val)}" id="#{build_html_multi_id(field_instance_id,val)}" type="checkbox" value="#{val}" #{checked.include?(val) ? 'checked' : ''} onClick="#{javascript}"> #{key}|
     end
     params = options[:params]
     if params 
@@ -17,7 +56,7 @@ class CheckBoxGroupWidget < Widget
     else
       result = result.join("\n")
     end
-    result << %Q|<input name="#{build_html_multi_name(field_instance_id,'__none__')}" id="#{build_html_multi_id(field_instance_id,'__none__')}" type="hidden"}>|
+    result + %Q|<input name="#{build_html_multi_name(field_instance_id,'__none__')}" id="#{build_html_multi_id(field_instance_id,'__none__')}" type="hidden"}>| +  "#{form.javascript_tag(js)}" 
   end
 
   def self.humanize_value(value,options=nil)
@@ -43,7 +82,7 @@ class CheckBoxGroupWidget < Widget
     e = enumeration(options[:constraints])
     result = ""
     e.each do |key,value|
-       result << %Q|var watcher_#{build_html_multi_id(field_instance_id,value)} = new WidgetWatcher('#{build_html_multi_id(field_instance_id,value)}', function(e){ #{script} });\n|
+       result << %Q|var watcher_#{build_html_multi_id(field_instance_id,value.chomp('*'))} = new WidgetWatcher('#{build_html_multi_id(field_instance_id,value.chomp('*'))}', function(e){ #{script} });\n|
     end
     result
   end
--- lib/metaform/widgets/check_box_group_followup.rb
+++ lib/metaform/widgets/check_box_group_followup.rb
@@ -4,57 +4,108 @@
 class CheckBoxGroupFollowupWidget < Widget
   ################################################################################
   def self.render_form_object(form,field_instance_id,value,options)
-
     e = enumeration(options[:constraints])
     result = []
     set_values = YAML.load(value) if value
     set_values ||= {}
-    
-    params = options[:params].split(/,/)
+    params =  options[:params].split(/,/)
     sub_label = params.shift
-    e.each do |value_label,val|
-      if val != 'none'
-        checked = set_values[val]
-        followup_id = "#{field_instance_id}_#{val}"
-
-        idx = "_#{val}-none"
-        none_checked = checked && checked.include?('none')
-        followups = [<<-EOHTML
-          <input name="#{build_html_multi_name(field_instance_id,idx)}" id="#{build_html_multi_id(field_instance_id,idx)}" type="checkbox" value="none" #{ none_checked ? 'checked' : ''}> None
-          EOHTML
-          ]
-        params.each do |i|
-          idx = "_#{val}-#{i}"
-          followups << <<-EOHTML
-          <input name="#{build_html_multi_name(field_instance_id,idx)}" id="#{build_html_multi_id(field_instance_id,idx)}" type="checkbox" value="#{i}" #{ (checked && checked.include?(i)) ? 'checked' : ''}> #{i.humanize}
-          EOHTML
-        end
+    none_fields_regular = []
+    e.map!{|value_label,val|
+      new_val = val.chomp('*')
+      if new_val != val || val == 'none'
+        none_fields_regular << new_val
       end
-
-      none_id = build_html_multi_id(field_instance_id,'none')
-      if val == 'none'
-        # Javscript: uncheck all items in this checkbox group if the users clicks on none and also hide the followup.
-        javascript = "if ($('#{none_id}').checked) {mapCheckboxGroup('#{build_html_name(field_instance_id)}',$('metaForm'),function(e,val){if (val != 'none') {e.checked=false};var followup_id='#{field_instance_id}_'+val;var h = $(followup_id);if (h != null) {h.hide()}})}"
-        followup_span = ''
+      [value_label,new_val]
+    }
+    none_fields_followup = []
+    params.map!{|val|
+      new_val = val.chomp('*')
+      if new_val != val || val == 'none'
+        none_fields_followup << new_val
+      end
+      new_val
+    }
+    js_none = ""
+    none_fields_regular.each { |none_val|
+      none_id = build_html_multi_id(field_instance_id,none_val)
+      js_none << <<-EOJS
+       if ($('#{none_id}')) {
+         $('#{none_id}').checked = false;
+       }
+       EOJS
+     }
+    js = <<-EOJS
+       function do_click_#{field_instance_id}_none(theCheckbox,theValue) {
+           if (theCheckbox.checked) {  
+             mapCheckboxGroup('#{build_html_name(field_instance_id)}',$('metaForm'),function(e,val){if (val != theValue) {e.checked=false};var followup_id='#{field_instance_id}_'+val;var h = $(followup_id);if (h != null) {Effect.BlindUp(h, {duration:.5})}})
+           }
+    		}  
+    		function do_click_#{field_instance_id}_regular(theCheckbox,theValue,theFollowupID) {
+          var e = $(theFollowupID); 
+          if (theCheckbox.checked) {
+            Effect.BlindDown(e, {duration:.5});
+            #{js_none}
+          } else {
+            Effect.BlindUp(e, {duration:.5});
+            mapCheckboxGroup('record[#{field_instance_id}][_'+theValue+'-',$('metaForm'),function(el,val){el.checked=false})
+          }           
+   		  }
+        
+    	EOJS
+    
+    e.each do |value_label,val|
+      checked = set_values[val]
+      if none_fields_regular.include?(val) 
+        followup_span = ''  # 'none' doesn't get followups
+        # Javscript: uncheck all items in this checkbox group if the users clicks on none and also hide all the followups.
+        javascript = "do_click_#{field_instance_id}_none(this,'#{val}')"
       else
-        # Javscript: hide/show the followup (unchecking all items in the followup if hiding, and unchecking the none value if showing)
-        javascript = "var e = $('#{followup_id}'); if (this.checked) {e.show();$('#{none_id}').checked = false} else {e.hide();mapCheckboxGroup('record[#{field_instance_id}][_#{val}-',$('metaForm'),function(el,val){el.checked=false})}"
+        followups = []
+        followup_id = "#{field_instance_id}_#{val}"
+        params.each do |param|
+          idx = "_#{val}-#{param}"
+          id = build_html_multi_id(field_instance_id,idx)
+          checked_string = (checked && checked.include?(param)) ? 'checked' : ''
+          if none_fields_followup.include?(param)
+            on_click_string = %Q|onClick="if ($('#{id}').checked) {mapCheckboxGroupFollowup('#{build_html_name(field_instance_id)}','_#{val}',$('metaForm'),function(e,val){if (val != '#{param}') {e.checked=false}})}"|
+          elsif none_fields_followup.length > 0
+            none_js = ''
+            none_fields_followup.each {
+              none_id = build_html_multi_id(field_instance_id,"_#{val}-#{none_fields_followup[0]}")
+              none_js << <<-EOJS
+                if ($('#{none_id}')) {
+                  $('#{none_id}').checked = false;
+                }              
+              EOJS
+              on_click_string = %Q|onClick="#{none_js}"| 
+            }
+          end
+          followups << <<-EOHTML
+          <input name="#{build_html_multi_name(field_instance_id,idx)}" id="#{id}" type="checkbox" value="#{param}" #{checked_string} #{on_click_string}> #{param.humanize}
+          EOHTML
+        end  
         followup_span = <<-EOHTML 
           <span id="#{followup_id}" class="checkbox_followups" style="display:#{checked ? 'inline' : 'none'}">
           &nbsp;&nbsp; #{sub_label} #{followups.join("\n")}
           </span>
         EOHTML
-      end
+        # Javscript: hide/show the followup (unchecking all items in the followup if hiding, and unchecking the none value if showing)
+        javascript = "do_click_#{field_instance_id}_regular(this,'#{val}','#{followup_id}')"
+        js << <<-EOJS
+       		do_click_#{field_instance_id}_regular($('#{build_html_multi_id(field_instance_id,val)}'),'#{val}','#{followup_id}');
+          EOJS
+      end  
       result << <<-EOHTML 
       <input name="#{build_html_multi_name(field_instance_id,'__none__')}" id="#{build_html_multi_id(field_instance_id,'__none__')}" type="hidden"}>
-      <input name="#{build_html_multi_name(field_instance_id,val)}" id="#{build_html_multi_id(field_instance_id,val)}" type="checkbox" value="#{val}" #{checked ? 'checked' : ''}
+      <span class="check_box_followup_input"><input name="#{build_html_multi_name(field_instance_id,val)}" id="#{build_html_multi_id(field_instance_id,val)}" type="checkbox" value="#{val}" #{checked ? 'checked' : ''}
         onClick="#{javascript}">
-        #{value_label}
+        #{value_label}</span>
         #{followup_span}
       EOHTML
     end
 
-    result.join("<br />")
+    result.join("<br />") + '<div class="clear"></div>' + "#{form.javascript_tag(js)}" 
     
   end
 
@@ -77,10 +128,13 @@ class CheckBoxGroupFollowupWidget < Widget
     e = enumeration(options[:constraints])
     result = ""
     e.each do |key,value|
-      result << %Q|Event.observe('#{build_html_multi_id(field_instance_id,value)}', 'change', function(e){ #{script} });\n|
-      params.each do |i|
-        idx = "_#{value}-#{i}"
-        result << %Q|Event.observe('#{build_html_multi_id(field_instance_id,idx)}', 'change', function(e){ #{script} });\n|
+      new_val = value.chomp('*')
+      result << %Q|Event.observe('#{build_html_multi_id(field_instance_id,new_val)}', 'change', function(e){ #{script} });\n|
+      if (new_val == value) && (new_val != 'none')
+        params.each do |i|
+          idx = "_#{new_val.chomp('*')}-#{i}"
+          result << %Q|Event.observe('#{build_html_multi_id(field_instance_id,idx)}', 'change', function(e){ #{script} });\n|
+        end
       end
     end
     result
