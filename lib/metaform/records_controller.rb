764f24ff14a7474026010c23cf3926626c650644
Eric Harris-Braun <eric@harris-braun.com>
2008-10-09 18:02:55 -0400

    added capcity by before_update and before_save callbacks to skip the update/save by returning true

--- lib/metaform/records_controller.rb
+++ lib/metaform/records_controller.rb
@@ -54,64 +54,67 @@ class RecordsController < ApplicationController
   # PUT /records/1.xml
   def update
     setup_record
-    before_update_record(@record) if respond_to?(:before_update_record)
-    before_save_record(@record) if respond_to?(:before_save_record)
-    respond_to do |format|
-      if !params[:record] && !params[:meta]
-        redirect_url = params[:_redirect_url] if params[:_redirect_url]
-        format.html { redirect_url ? redirect_to(redirect_url) : render(:action => "show") }
-        format.xml  { head :ok }
-      else
-        opts = {:convert_from_html=>true}
-        if @index
-          attribs = params[:record]
-          opts[:index] = @index
-        elsif params[:multi_index]
-          opts[:multi_index] = true
-          attrs = []
-          zap_fields = params[:multi_index_fields].split(/,/)
-          attribs = {0=>{}}
-          params[:record].each do |k,v|
-            if k =~ /_([0-9]+)_(.*)/
-              idx = $1.to_i
-              fn = $2
-#              zap_fields << fn
-              attrs[idx] ||= {}
-              attrs[idx][fn] = v
-            else
-              attribs[0][k] = v
-            end
-          end
-
-          # compact all the attributes into a hash ignoring the actual index given
-          # this handles all the issues of deleting indexes
-          attrs = attrs.compact
-          # first merge the 0th items into attribs (because there could have been other)
-          # non indexed items on the page at the 0th level
-          if attrs[0]
-            attribs[0].update(attrs[0])
-            attrs.shift
-          end
-          #then copy in any indexed items
-          x = 1
-          attrs.each {|a| attribs[x]=a;x+=1}
-          opts[:clear_indexes] = zap_fields
-        else
-          attribs = params[:record]
-        end
-        meta_data = get_meta_data
-        meta_data[:explanations] = params[:explanations] if params[:explanations]
-        if saved_attributes = @record.update_attributes(attribs,@presentation,meta_data,opts)
-          after_update_record(@record) if respond_to?(:after_update_record)
-          after_save_record(@record,saved_attributes) if respond_to?(:after_save_record)
-          flash[:action_result] = @record.action_result[:return_data] if @record.action_result && @record.action_result[:return_data]
-          redirect_url = @record.action_result[:redirect_url] if @record.action_result
-          redirect_url = params[:_redirect_url] if !redirect_url  && params[:_redirect_url]
-          format.html { redirect_url ? redirect_to(redirect_url) : render_show }
+    redirected = false
+    redirected = before_update_record(@record) if respond_to?(:before_update_record)
+    redirected = before_save_record(@record) if respond_to?(:before_save_record) && !redirected
+    if !redirected
+      respond_to do |format|
+        if !params[:record] && !params[:meta]
+          redirect_url = params[:_redirect_url] if params[:_redirect_url]
+          format.html { redirect_url ? redirect_to(redirect_url) : render(:action => "show") }
           format.xml  { head :ok }
         else
-          format.html { render_show }
-          format.xml  { render :xml => @updated.errors.to_xml }
+          opts = {:convert_from_html=>true}
+          if @index
+            attribs = params[:record]
+            opts[:index] = @index
+          elsif params[:multi_index]
+            opts[:multi_index] = true
+            attrs = []
+            zap_fields = params[:multi_index_fields].split(/,/)
+            attribs = {0=>{}}
+            params[:record].each do |k,v|
+              if k =~ /_([0-9]+)_(.*)/
+                idx = $1.to_i
+                fn = $2
+  #              zap_fields << fn
+                attrs[idx] ||= {}
+                attrs[idx][fn] = v
+              else
+                attribs[0][k] = v
+              end
+            end
+
+            # compact all the attributes into a hash ignoring the actual index given
+            # this handles all the issues of deleting indexes
+            attrs = attrs.compact
+            # first merge the 0th items into attribs (because there could have been other)
+            # non indexed items on the page at the 0th level
+            if attrs[0]
+              attribs[0].update(attrs[0])
+              attrs.shift
+            end
+            #then copy in any indexed items
+            x = 1
+            attrs.each {|a| attribs[x]=a;x+=1}
+            opts[:clear_indexes] = zap_fields
+          else
+            attribs = params[:record]
+          end
+          meta_data = get_meta_data
+          meta_data[:explanations] = params[:explanations] if params[:explanations]
+          if saved_attributes = @record.update_attributes(attribs,@presentation,meta_data,opts)
+            after_update_record(@record) if respond_to?(:after_update_record)
+            after_save_record(@record,saved_attributes) if respond_to?(:after_save_record)
+            flash[:action_result] = @record.action_result[:return_data] if @record.action_result && @record.action_result[:return_data]
+            redirect_url = @record.action_result[:redirect_url] if @record.action_result
+            redirect_url = params[:_redirect_url] if !redirect_url  && params[:_redirect_url]
+            format.html { redirect_url ? redirect_to(redirect_url) : render_show }
+            format.xml  { head :ok }
+          else
+            format.html { render_show }
+            format.xml  { render :xml => @updated.errors.to_xml }
+          end
         end
       end
     end
