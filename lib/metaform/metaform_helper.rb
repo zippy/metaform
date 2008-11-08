896f2994475bd5a12b738a6f927245749303a8d8
Lisa Sawin <lsawin@optonline.net>
2008-11-08 16:22:39 -0500

    metaform.css now applies to printing

--- lib/metaform/metaform_helper.rb
+++ lib/metaform/metaform_helper.rb
@@ -1,7 +1,7 @@
 module MetaformHelper
   def include_metaform_assets
     if @metaform_include_assets
-	    javascript_include_tag('metaform') << stylesheet_link_tag('metaform')
+	    javascript_include_tag('metaform') << stylesheet_link_tag('metaform', :media => "all")
     else
       ''
     end
