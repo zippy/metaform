f7efd3a46d70bfd2d8570f8805c4eec9de0cbd74
Eric Harris-Braun <eric@harris-braun.com>
2009-01-24 00:44:44 -0500

    updated metaform to work with rails 2.2.2

--- init.rb
+++ init.rb
@@ -1,3 +1,3 @@
-Dependencies.load_once_paths -= Dependencies.load_once_paths.select{|path| path =~ %r(^#{File.dirname(__FILE__)}) }
+ActiveSupport::Dependencies.load_once_paths -= ActiveSupport::Dependencies.load_once_paths.select{|path| path =~ %r(^#{File.dirname(__FILE__)}) }
 require 'metaform'
 ActionView::Base.send :include, MetaformHelper
