7ba64a7945026bb6d7897ecb0e95f4242cede781
Lisa Sawin <lsawin@optonline.net>
2009-01-28 09:17:34 -0500

    Prior to Rails 2.2.2, need to call ActiveSupport::Dependencies directly, without the preceeding ActiveSupport

--- init.rb
+++ init.rb
@@ -1,3 +1,8 @@
-ActiveSupport::Dependencies.load_once_paths -= ActiveSupport::Dependencies.load_once_paths.select{|path| path =~ %r(^#{File.dirname(__FILE__)}) }
+if defined?(ActiveSupport::Dependencies)
+  ActiveSupport::Dependencies.load_once_paths -= ActiveSupport::Dependencies.load_once_paths.select{|path| path =~ %r(^#{File.dirname(__FILE__)}) }
+else
+  #Prior to Rails 2.2.2 (?) we call Dependencies directly, without ActiveSupport.
+  Dependencies.load_once_paths -= Dependencies.load_once_paths.select{|path| path =~ %r(^#{File.dirname(__FILE__)})}
+end
 require 'metaform'
 ActionView::Base.send :include, MetaformHelper
