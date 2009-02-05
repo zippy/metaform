c2c1f1094b9aaf0666778f0f48b3f3d17cc51479
Eric Harris-Braun <eric@harris-braun.com>
2009-02-05 01:15:55 -0500

    fixed bugs in metaform_helper specs

--- spec/spec/metaform_helper_spec.rb
+++ spec/spec/metaform_helper_spec.rb
@@ -35,13 +35,13 @@ describe MetaformHelper do
     def_search_rule('e') {|search_for| ":email like '%#{search_for}%'"}
     def_search_rule('w',:meta_condition=>true) {|search_for| "workflow_state = '#{search_for}'"}
     @search_params = {'on_name'=>'n','for_name'=>'bob','on_email'=>'e','for_email'=>'herb','on_w'=>'w','for_w'=>'claimed'}
-    generate_search_options(:search).should == {:meta_condition=>"(workflow_state = 'claimed')", :conditions=>["(:name = 'bob')", "(:email like '%herb%')"]}
+    generate_search_options(:search).should == {:meta_condition=>"(workflow_state = 'claimed')", :conditions=>"(:name = 'bob') and (:email like '%herb%')"}
   end
 
   it 'should produce search parameters for Record.search with | search_for' do
     def_search_rule('n') {|search_for| ":name = '#{search_for}'"}
     @search_params = {'on_name'=>'n','for_name'=>'bob|herb'}
-    generate_search_options(:search).should == {:conditions => ["(:name = 'bob' or :name = 'herb')"]}
+    generate_search_options(:search).should == {:conditions => "(:name = 'bob' or :name = 'herb')"}
   end
 
   it 'should produce search parameters for Record.search with multiple meta_conditions' do
@@ -68,12 +68,12 @@ describe MetaformHelper do
   it 'should generate default search rules' do
     def_search_rules(:search,'birth_code' => 'H_Ccode')
     @search_params = {'on_w'=>'birth_code_is','for_w'=>'ABCD'}
-    generate_search_options(:search).should == {:conditions=>["(:H_Ccode = 'ABCD')"]}
+    generate_search_options(:search).should == {:conditions=>"(:H_Ccode = 'ABCD')"}
     @search_params = {'on_w'=>'birth_code_not','for_w'=>'ABCD'}
-    generate_search_options(:search).should == {:conditions=>["not (:H_Ccode = 'ABCD')"]}
+    generate_search_options(:search).should == {:conditions=>"not (:H_Ccode = 'ABCD')"}
     @search_params = {'on_w'=>'birth_code_b','for_w'=>'ABCD'}
-    generate_search_options(:search).should == {:conditions=>["(:H_Ccode like 'ABCD%')"]}
+    generate_search_options(:search).should == {:conditions=>"(:H_Ccode ilike 'ABCD%')"}
     @search_params = {'on_w'=>'birth_code_c','for_w'=>'ABCD'}
-    generate_search_options(:search).should == {:conditions=>["(:H_Ccode like '%ABCD%')"]}
+    generate_search_options(:search).should == {:conditions=>"(:H_Ccode ilike '%ABCD%')"}
   end
 end
