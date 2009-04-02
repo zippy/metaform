9b0b6609bb57679c4437d724914b170ce9bd44dc
Lisa Sawin <lsawin@optonline.net>
2009-04-02 09:35:15 -0400

    Revert to old listing spec

--- spec/metaform/listing_spec.rb
+++ spec/metaform/listing_spec.rb
@@ -3,9 +3,9 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe Listings do
   before(:each) do
     Record.make(SampleForm.new,'new_entry',{:name =>'Bob Smith',:fruit => 'banana',:occupation => 'unemployed'}).save('new_entry')
-    Record.make(SampleForm.new,'new_entry',{:name =>'Joe Doe',:fruit => 'apple',:occupation => 'unemployed'}).save('new_entry')
+    Record.make(SampleForm.new,'new_entry',{:name =>'Joe Smith',:fruit => 'apple',:occupation => 'unemployed'}).save('new_entry')
     Record.make(SampleForm.new,'new_entry',{:name =>'Will Smith',:fruit => 'apple_mutsu',:occupation => 'unemployed'}).save('new_entry')
-    r = Record.make(SampleForm.new,'new_entry',{:name =>'Jane Jones',:fruit => 'banana',:occupation => 'unemployed'})
+    r = Record.make(SampleForm.new,'new_entry',{:name =>'Jane Smith',:fruit => 'banana',:occupation => 'unemployed'})
     r[:occupation,1] = 'farmer'
     r.occupation__2 = 'painter'
     r.save('new_entry')
@@ -14,16 +14,4 @@ describe Listings do
     @list = Log.get_list('samples')
     @list.size.should == 4
   end
-  # it "should return all items when listing has no search rules defined" do
-  #   @list = Log.get_listing('snapshot')
-  #   @list.size.should == 4
-  # end
-  # it "should return correct number of items when restricted by a search rule" do
-  #   @search_params = {"on_main"=>"n","for_main"=>"Smith"}
-  #   @list = Log.get_listing('restricted_snapshot')
-  #   @list.size.should == 4
-  # end
-  
-  #order
-  #search (pass in search params)
 end
