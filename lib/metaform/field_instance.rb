zippy/metaform

commit c82b8367203a2905bc21f65082e59c0400e26d41

Attempts to create duplicate field_instances for the same field_id form_instance and index now have a better error message

lib/metaform/field_instance.rb
@@ -1,16 +1,22 @@
 class FieldInstance < ActiveRecord::Base
   belongs_to :form_instance
 #  belongs_to :field
-  validates_uniqueness_of :field_id, :scope => [:form_instance_id,:idx]
   validates_presence_of :field_id,:form_instance_id
 
   States = %w(unanswered answered invalid explained calculated approved)
   validates_inclusion_of :state, :in => States
+
+  # Instead of standard use of validates_uniqueness_of which would be:
+  #  validates_uniqueness_of :field_id, :scope => [:form_instance_id,:idx], :message => Proc.new {|x| "XIPYS"}
+  # we customize it to produce a dynamic message based on the the field id
+  def validate
+    FieldInstance.validates_uniqueness_of :field_id, :scope => [:form_instance_id,:idx],:message => "attempt to create duplicate field instance for: #{self.field_id}"
+  end
     
   def field
     form_instance.form.field_exists?(field_id)
   end
-  
+    
   #TODO figure out if we want to do rails level validation of field instances...
   def validatex
     logger.info "fish"

spec/metaform/field_instances_spec.rb
@@ -1,0 +1,17 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe FieldInstance do
+  it "should complain when creating duplicate field instances" do
+    f = FieldInstance.new({:field_id=>'x', :form_instance_id => 1, :idx => 0,:state => 'answered'})
+    f.save.should == true
+    f2 = FieldInstance.new({:field_id=>'x', :form_instance_id => 1, :idx => 0,:state => 'answered'})
+    f2.save.should == false
+    f2.errors.on(:field_id).should == "attempt to create duplicate field instance for: x"
+    f = FieldInstance.new({:field_id=>'x', :form_instance_id => 1, :idx => 1,:state => 'answered'})
+    f.save.should == true
+    f = FieldInstance.new({:field_id=>'y', :form_instance_id => 1, :idx => 0,:state => 'answered'})
+    f.save.should == true
+    f = FieldInstance.new({:field_id=>'x', :form_instance_id => 2, :idx => 0,:state => 'answered'})
+    f.save.should == true
+  end
+end

commit cce7cf311b7166bcede8dec4f77afbe41624e770

reversed c82b8367 which introduced major speed problems and solved the problem in record#update_attributes instead of in form_instances

lib/metaform/field_instance.rb
@@ -6,12 +6,7 @@ class FieldInstance < ActiveRecord::Base
   States = %w(unanswered answered invalid explained calculated approved)
   validates_inclusion_of :state, :in => States
 
-  # Instead of standard use of validates_uniqueness_of which would be:
-  #  validates_uniqueness_of :field_id, :scope => [:form_instance_id,:idx], :message => Proc.new {|x| "XIPYS"}
-  # we customize it to produce a dynamic message based on the the field id
-  def validate
-    FieldInstance.validates_uniqueness_of :field_id, :scope => [:form_instance_id,:idx],:message => "attempt to create duplicate field instance for: #{self.field_id}"
-  end
+  validates_uniqueness_of :field_id, :scope => [:form_instance_id,:idx]
     
   def field
     form_instance.form.field_exists?(field_id)

lib/metaform/record.rb
@@ -732,8 +732,8 @@ end
               i.delete unless i.new_record?
             else
               puts "<br>about to save #{i.attributes.inspect}" if DEBUG1
-              if !i.save!
-                errors.add(i.field_id,i.errors.full_messages.join(','))
+              if !i.save
+                raise "ActiveRecord error:'#{i.errors.full_messages.join(',')}' encountered when saving #{i.field_id}"
               end
             end
           end

spec/metaform/field_instances_spec.rb
@@ -1,17 +1,0 @@
-require File.dirname(__FILE__) + '/../spec_helper'
-
-describe FieldInstance do
-  it "should complain when creating duplicate field instances" do
-    f = FieldInstance.new({:field_id=>'x', :form_instance_id => 1, :idx => 0,:state => 'answered'})
-    f.save.should == true
-    f2 = FieldInstance.new({:field_id=>'x', :form_instance_id => 1, :idx => 0,:state => 'answered'})
-    f2.save.should == false
-    f2.errors.on(:field_id).should == "attempt to create duplicate field instance for: x"
-    f = FieldInstance.new({:field_id=>'x', :form_instance_id => 1, :idx => 1,:state => 'answered'})
-    f.save.should == true
-    f = FieldInstance.new({:field_id=>'y', :form_instance_id => 1, :idx => 0,:state => 'answered'})
-    f.save.should == true
-    f = FieldInstance.new({:field_id=>'x', :form_instance_id => 2, :idx => 0,:state => 'answered'})
-    f.save.should == true
-  end
-end